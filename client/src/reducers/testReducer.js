cda89bf9989afa96d713d7376073a37c71423db3
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2019-01-11 10:21:07 -0500

    To /reducers, add rootReducer and testReducer
    Based off Browseum.

---
+++ client/src/reducers/testReducer.js
@@ -0,0 +1,10 @@
+function testReducer(state = [], action) {
+  switch (action.type) {
+    // case 'DO_SOMETHING':
+    //   return [...action.payload]
+    default:
+      return state
+  }
+}
+
+export default testReducer
